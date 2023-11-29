64ebe415c93de2de0ffa53e7a9f71c878ddcfeb9
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2023-11-29 20:59:16 +0300

    create file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
-# create a file in /tmp
-file { '/tmp/school':
-    path    => '/tmp/school',
-    mode    => '0744',
-    owner   => 'www-data',
-    group   => 'www-data',
-    content => 'I love puppet',
+# creates a file in /tmp
+
+file {'/tmp/school':
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
 }
