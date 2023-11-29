4904d2fbb98c0c64d7473946b6b16a0ad2db4eaf
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2023-11-29 21:23:54 +0300

    install flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,7 @@
-# installs flask
+# installs puppet-lint package
 
 package {'flask':
-    ensure   => '2.1.0',
-    provider => 'pip3',
-    name     => 'flask',
+  ensure   => '2.1.0',
+  name     => 'flask',
+  provider => 'pip3'
 }
