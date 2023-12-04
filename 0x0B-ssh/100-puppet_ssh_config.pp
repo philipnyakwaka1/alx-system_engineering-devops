5e6602a36543030d24e326f621d42ecc81c9e0c3
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2023-12-04 21:40:53 +0300

    puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,10 +2,12 @@
 
 file_line { 'Turn off passwd auth':
   path   => '/etc/ssh/sshd_config',
-  line   => 'PasswordAuthentication no'
+  line   => 'PasswordAuthentication no',
+  match  => '^#?PasswordAuthentication',
 }
 
 file_line { 'Declare identity file':
   path   => '/etc/ssh/ssh_config',
-  line   => 'IdentityFile ~/.ssh/school'
+  line   => 'IdentityFile ~/.ssh/school',
+  match  => '^#?IdentityFile',
 }
