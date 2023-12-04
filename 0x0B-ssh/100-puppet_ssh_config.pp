dd4c96557f6a82ee2196b7a87dbe1b8edc27522e
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2023-12-04 21:42:46 +0300

    puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -9,5 +9,5 @@ file_line { 'Turn off passwd auth':
 file_line { 'Declare identity file':
   path   => '/etc/ssh/ssh_config',
   line   => 'IdentityFile ~/.ssh/school',
-  match  => '^#?IdentityFile',
+  match  => '^#?IdentityFile.*~/.ssh/school',
 }
