3920d6a12a252c43bf97fc4ed89b9db9651a5aa0
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2024-01-19 15:29:13 +0300

    WPS

---
+++ 0x18-webstack_monitoring/test_datadog_wps_monitor.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""requests datadog api"""
+import requests
+import json
+
+headers = {
+    "Content-Type": "application/json",
+    "DD-API-KEY": 'e0b05777fb20cda8f77db622e1231fc6',
+    "DD-APPLICATION-KEY": '884a9183c461eef2650482beabab7f72e05400a6',
+}
+
+response = requests.get(
+    'https://api.datadoghq.com/api/v1/monitor', headers=headers)
+
+if response.status_code == 200:
+    monitors = json.loads(response.text)
+    for monitor in monitors:
+        if monitor.get('name') == 'WRITES PER SECOND':
+            print('WPS is monitored')
