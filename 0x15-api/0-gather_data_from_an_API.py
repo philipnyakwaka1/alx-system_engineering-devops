4e797705a34ef1624c08b483c0d224aaa2498301
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2024-01-19 17:07:50 +0300

    todos

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Python script that, using this REST API, for a given employee ID,
+returns information about his/her TODO list progress
+"""
+import requests
+import sys
+import json
+
+url_n = f'https://jsonplaceholder.typicode.com/users/{sys.argv[1]}/'
+name = json.loads(requests.get(url_n).text).get('name')
+print(name)
+response = requests.get('https://jsonplaceholder.typicode.com/todos/')
+task_list = json.loads(response.text)
+completed = 0
+total = 0
+titles = []
+for task in task_list:
+    if int(task.get('userId')) == int(sys.argv[1]):
+        if task.get('completed') is True:
+            titles.append(task.get('title'))
+            completed += 1
+            total += 1
+        else:
+            total += 1
+print(f'Employee {name} is done with tasks({completed}/{total})')
+for title in titles:
+    print(f'\t{title}')
---
+++ 0x15-api/test.py
@@ -0,0 +1,4 @@
+my_dict = {'age': 25}
+my_dict['age'] += 1
+my_dict['name'] = 'philip'
+print(my_dict)
