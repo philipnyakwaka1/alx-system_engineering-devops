45b68b52adaa341c782ae5c3be4ae268c546bd75
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2024-01-19 17:22:33 +0300

    added

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,9 +3,9 @@
 Python script that, using this REST API, for a given employee ID,
 returns information about his/her TODO list progress
 """
+import json
 import requests
 import sys
-import json
 
 
 def my_func():
