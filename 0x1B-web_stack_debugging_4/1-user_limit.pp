9d6689fe5a94c617ace10395808d826e1637a25c
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2024-01-31 20:29:39 +0300

    descriptors

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,9 @@
+exec { 'fix1':
+  command => "sed -i '/^holberton hard/s/5/50000/' /etc/security/limits.conf",
+  path    => '/usr/local/bin/:/bin/'
+}
+
+exec { 'fix2':
+  command => 'sed -i "/^holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
